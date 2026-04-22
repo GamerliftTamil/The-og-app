094eb486c8bcfc74dc7fa30ad4864e8c093488d4
pranay aby <pranayabyab@gmail.com>
2026-04-22 16:56:06 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/save', methods=['POST'])
+def save():
+    data = request.json
+
+    name = data.get("name")
+    email = data.get("email")
+
+    with open("userdata.txt", "a") as file:
+        file.write(f"Name: {name}, Email: {email}\n")
+
+    return "Data saved successfully"
+
+if __name__ == "__main__":
+    app.run(debug=True)
